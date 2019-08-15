281ed813a4309bed7e50d8577d276b1c91ede972
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-15 01:07:52 -0300

    Fixed chart default hight with styles prop; Make chart increase height together with data insertion; Chart labels can be hidden onClick; removePerson is now asyncronous and emits a 'remove' event; dataCollection method renamed to handleQueryData; Handle overallParticipation error on array reduce when dataset data is an empty array; Local numPersons variable to control some visual behaviors; numLabels variable to control chart height increasing; handleQueryData now emits a 'queryLoaded' event and can be used to transfer data between components; handleResize method added to control chart's height and width variation when resize window event is triggered; computed property myStyles to keep track of the style properties that are going to be sent to the chart component; Added window resize event listener on mounted and remove it on destroyed; update's property watcher is now an async function that awaits findPerson query refetch, it calls the handleRisize method to update the chart when new data is loaded; many layout and visual tweaks, table now have a slide out element with some usage hints and suggestions

--- frontend/components/ListPersons.vue
+++ frontend/components/ListPersons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="table">
+    <div class="table-container">
 
       <table>
 
@@ -14,20 +14,35 @@
         </thead>
 
         <tbody>
-          <tr @dblclick="removePerson(person.id)" v-for="(person, index) in findPersons" :key="person.id">
+          <tr 
+            @dblclick="removePerson(person.id)" 
+            v-for="(person, index) in findPersons" 
+            :key="person.id"
+          >
             <td>{{ index+1 }}</td>
             <td class="left">{{ person.firstName }}</td>
             <td class="left">{{ person.lastName }}</td>
             <td>{{ person.participation }}%</td>
+            
           </tr>
         </tbody>
-
+          
       </table>
-        
+      <div class="wrapper">
+        <span v-if="numPersons>0" class="slide">Double-click to remove person</span>
+        <span v-if="numPersons===0" class="slide">Empty database, please insert new persons</span>
+      </div>
     </div>
 
-    <div class="chart">
-      <ch-doughnut :chart-data="dataCollection(findPersons)" :options="chartOptions"/>
+    <div class="chart-container">
+
+      <!-- Chart component -->
+      <ch-doughnut 
+        :styles="myStyles" 
+        :chart-data="handleQueryData(findPersons)" 
+        :options="chartOptions"
+      />
+
     </div> 
 
   </div>    
@@ -56,41 +71,76 @@ export default {
   data() {
 
     return {
+
+      overallParticipation: 0,
+      numPersons: 0,
+      numLabels: 0,
+
+      // Initial chart values
+      chart: {
+        height: 230,
+        width: 600,
+      },
+
       chartOptions: {
 
         // Chart config
-        maintainAspectRatio: true,        
+        maintainAspectRatio: false,        
         responsive: true,
+        hover: {
+          mode: 'single'
+        },
 
         // Legend config
         legend: {
 
           display: true,
           position: 'right',
-          align: 'center',
           
           labels: {
 
-            boxWidth: 12,
+            boxWidth: 15,
             padding: 20,
+            fontSize: 15,
+
             generateLabels(chart){
 
               let labels = [];
               let labelsText = chart.data.labels;
+              for (let i in labelsText) {
+                let meta = chart.getDatasetMeta(0);
 
-              for (let key in labelsText) {
                 labels.push({
-                  text: '  ' + labelsText[key],
-                  fillStyle: chart.data.datasets[0].backgroundColor[key],
+                  text: '  ' + labelsText[i],
+                  fillStyle: chart.data.datasets[0].backgroundColor[i],
                   lineCap: 'round',
                   lineWidth: 10,
-                  strokeStyle: chart.data.datasets[0].backgroundColor[key],
-                  fontColor: chart.data.datasets[0].backgroundColor[key]
+                  lineJoin: 'round',
+                  strokeStyle: chart.data.datasets[0].backgroundColor[i],
+                  hidden: isNaN(chart.data.datasets[0].data[i]) || meta.data[i].hidden,
+
+                  index: i
                 });
               }
 
               return labels
             }
+            
+          },
+          onClick: function(e, legendItem) {
+            let index = legendItem.index;
+            let chart = this.chart;
+            let i, ilen, meta;
+
+            for (i = 0, ilen = (chart.data.datasets || []).length; i < ilen; ++i) {
+              meta = chart.getDatasetMeta(i);
+              // toggle visibility of index if exists
+              if (meta.data[index]) {
+                meta.data[index].hidden = !meta.data[index].hidden;
+              }
+            }
+
+            chart.update();
           }
         }
 
@@ -100,22 +150,22 @@ export default {
   },
 
   methods: {
-    
 
-    removePerson(personId){
-      this.$apollo.mutate({
+    // Remove person, refetch findPersons query and emit a 'remove' event
+    async removePerson(personId){
+      await this.$apollo.mutate({
         mutation: removePerson,
         variables: {
           id: personId
         }
       });
       this.$apollo.queries.findPersons.refetch();
+      this.$emit('remove');
     },
 
-    
-
-    // Adjust data items to Chatjs' data format
-    dataCollection(queryData){
+    // Adjust query data items to Chatjs' data format and set update some local variables
+    // This method is called on every time findPersons query loads
+    handleQueryData(queryData){
       let data = {
         labels: [],
         datasets: []
@@ -133,35 +183,86 @@ export default {
         dataset.data.push(person.participation);
       }
 
-      // Get the total participation sum
-      let overallParticipation = dataset.data
-        .reduce((accumulator, value) => accumulator + value);
-
+      let overallParticipation;
+      // Handle empty database
+      try {
+        // Get the total participation
+        overallParticipation = dataset.data
+          .reduce((accumulator, value) => accumulator + value);
+      } catch (e) {
+        overallParticipation = 0;
+      }
+      
       // Apply a color to each label
       dataset.backgroundColor = palette('mpn65', dataset.data.length)
         .map(hex => '#' + hex);
 
+      this.numPersons = dataset.data.length;
+
       // Create new label with dark grey background color if participation is lower than 100
       if (overallParticipation < 100) {
+
+        // Remaining participation
         let notAssigned = 100 - overallParticipation;
         
         data.labels.push('Not Assigned');
         dataset.data.push(notAssigned);
         dataset.backgroundColor.push('rgb(90, 90, 90)');
-      }      
+      }
+
+      this.numLabels = dataset.data.length;
+      this.overallParticipation = overallParticipation;
+
+      // Send overallParticipation value to parent component
+      this.$emit('queryLoaded', overallParticipation);
 
       data.datasets.push(dataset);
 
       return data;
+    },
+
+    // Control chart width and height values
+    handleResize() {
+      // Width
+      if (window.innerWidth > 1280){
+        this.chart.width = window.innerWidth*.30;
+      } else if (window.innerWidth <= 600 && window.innerWidth >= 300 ) {
+        this.chart.width = window.innerWidth;
+      }
+
+      // Height: 
+      if (this.numLabels > 6){
+        this.chart.height = 230 + 35*(this.numLabels - 6);
+      }
     }
+  },
+
+  computed: {
+    // Styling object passed to chart component
+    myStyles () {
+      return {
+        height: `${this.chart.height}px`,
+        width: `${this.chart.width}px`,
+        position: 'relative'
+      }
+    }
+  },
+
+  mounted() {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
   },
 
   watch: { 
     // Refetch query when on new Person input
-    update: function(value, oldValue) { 
+    update: async function(value, oldValue) { 
       if (value) {
+        await this.$apollo.queries.findPersons.refetch();
+        this.handleResize();
         this.$emit('updated');
-        this.$apollo.queries.findPersons.refetch();
       }
     }
   }
@@ -179,27 +280,64 @@ export default {
   align-content: center;
   width: 100%;
 }
-.table, .chart{
-  margin-right: 50px;
-  margin-left: 50px;
-}
-.chart{
-  height: 300px;
+.table-container, .chart-container{
+  margin-right: 60px;
+  margin-left: 60px;
+  margin-top: 60px;
 }
 table {
   border-collapse: collapse;
 }
 table, th, td {
   padding: 10px 13px 10px 13px;
-  border: 1px solid black;
-  border-color: #b5b5b5;
+
   color: #707070;
   font-size: 14px;
   text-align: center;
+}
+td, th{
+  border: 1px solid black;
+  border-color: #b5b5b5;
 }
 tr .left {
   text-align: left;
   padding: 10px 100px 10px 13px;
 }
+@media only screen and (min-width: 520px) {
+  table {width: 500px}
+}
+@media only screen and (min-width: 860px) {
+  table {width: 550px}
+}
 
+.wrapper {
+    position: relative;
+    overflow: hidden;
+    align-content: center;
+    width: 0 auto;
+    margin: 0 15px 0 15px;
+    height: 25px; 
+}
+
+.slide {
+    position: absolute;
+    top: -100px;
+    width: 100%;
+    height: 100%;
+    transition: 1s ease-in;
+    padding: 4px;
+    background-color:rgb(105, 105, 105);
+    border-radius: 0px 0px 5px 5px;
+    align-content: center;
+    text-align: center;
+    font-weight: 200;
+    font-size: 14px;
+    color: hsla(0, 0%, 100%, 0.97);
+    vertical-align: center;
+}
+
+.table-container:hover .slide {
+    transition: .1s;
+    top: 0;
+}
 </style>
