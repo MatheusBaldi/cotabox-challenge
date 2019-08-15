2ca718611ce1fa4fc4cb75139fbd5fca8f443834
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-15 00:50:15 -0300

    Add input validation with Vuelidate; Improve usage experience and visual layout; Set to receive overallParticipation and use it on validation

--- frontend/components/CreatePerson.vue
+++ frontend/components/CreatePerson.vue
@@ -1,48 +1,158 @@
 <template>
   <div class="header">
-    <ApolloMutation
-      :mutation="require('../apollo/mutations/addPerson.gql')"
-      :variables="{
-        firstName,
-        lastName,
-        participation
-      }"
-      @done="onDone()"
-    >
 
-      <template :disabled="loading" v-slot="{ mutate, loading, error }">
-        
-        <div @keypress.enter="send(mutate)" class="container">
-            <input  placeholder="First name" v-model="firstName" required/>
-            <input  placeholder="Last name" v-model="lastName" required/>
-            <input  placeholder="Participation" v-model.number="participation" required/>
-            <button @click.prevent="send(mutate)" class="btn" plain>SEND</button>
 
-            <p v-if="error">An error occurred: {{ error }}</p>
-        </div> 
+      <ApolloMutation
+        :mutation="require('../apollo/mutations/addPerson.gql')"
+        :variables="{
+          firstName,
+          lastName,
+          participation
+        }"
+        @done="onDone()"
+      >
 
-      </template>
+        <template :disabled="loading" v-slot="{ mutate, loading, error }">
+          
+          <div @keypress.enter="send(mutate)" class="container">
 
-    </ApolloMutation>
+              <div class="form-group" :class="{ 'form-group--error': $v.firstName.$error }">
+                
+                <!-- First Name Input -->
+                <input  class="form_input" placeholder="First name" v-model.lazy="$v.firstName.$model" required/>
+                
+                <!-- Required error -->
+                <div class="error" v-if="!$v.firstName.required && $v.firstName.$error">First name is required</div>
+                
+                <!-- Min characters error -->
+                <div class="error" v-if="!$v.firstName.minLength && $v.firstName.$error">Name must have at least {{$v.firstName.$params.minLength.min}} letters.</div>
+                
+                <!-- Max characters error -->
+                <div class="error" v-if="!$v.firstName.maxLength && $v.firstName.$error">Name must have less than {{$v.firstName.$params.maxLength.max}} letters.</div>
+              
+              </div>  
+              
+
+              <div class="form-group" :class="{ 'form-group--error': $v.lastName.$error }">
+              
+                <!-- Last Name Input -->
+                <input class="form_input" placeholder="Last name" v-model.lazy="$v.lastName.$model" required/>
+                
+                <!-- Required error -->
+                <div class="error" v-if="!$v.lastName.required && $v.lastName.$error">
+                  Last name is required
+                </div>
+
+                <!-- Min characters error -->
+                <div class="error" v-if="!$v.lastName.minLength && $v.lastName.$error">
+                  Name must have at least {{$v.lastName.$params.minLength.min}} letters.
+                </div>
+
+                <!-- Max characters error -->
+                <div class="error" v-if="!$v.lastName.maxLength && $v.lastName.$error">
+                  Name must have less than {{$v.lastName.$params.maxLength.max}} letters.
+                </div>
+
+              </div>  
+
+
+              <div class="form-group" :class="{ 'form-group--error': $v.participation.$error }">
+                
+                <!-- Participation Input -->
+                <input class="form_input" placeholder="Participation" v-model.number.lazy="$v.participation.$model" required/>
+                
+                <!-- Required error -->
+                <div class="error" v-if="!$v.participation.required && $v.participation.$error">
+                  Participation is required
+                </div>
+                
+                <!-- Default min and max values error -->
+                <div class="error" v-if="(!$v.participation.between || !$v.participation.integer) && $v.participation.$error">
+                  Participation must be an integer between {{$v.participation.$params.between.min}} and {{$v.participation.$params.between.max}}.
+                </div>
+
+                <!-- Dynamic max value error -->
+                <div class="error" v-if="!$v.participation.maxValue && $v.participation.$error">
+                  Total participation is currently {{ overallParticipation }}%, please insert a value under {{ 100 - overallParticipation }}.
+                </div>
+
+              </div>
+
+              <div class="form-control">
+                <button 
+                  :disable="$v.firstName.$invalid || $v.lastName.$invalid || $v.participation.$invalid" 
+                  @click.prevent="send(mutate)" 
+                  class="btn" 
+                  plain
+                >
+                  SEND
+                </button>
+              </div>
+          </div> 
+
+          <div class="error">{{ error }}</div>
+
+        </template>
+
+      </ApolloMutation>
+
+
   </div>
 </template>
 
 <script>
+import { required, integer, maxValue, minLength, maxLength, between } from 'vuelidate/lib/validators'
+
 export default {
+  props: [
+    'overallParticipation'
+  ],
+
+  // Local state
   data() {
     return {
       firstName: '',
       lastName: '',
-      participation: ''
+      participation: '',
+    }
+  },
+
+  // Vuelidate rules
+  validations() {
+    return {
+      firstName: {
+        required,
+        maxLength: maxLength(30),
+        minLength: minLength(1)
+      },
+      lastName: {
+        required,
+        maxLength: maxLength(30),
+        minLength: minLength(1)
+      },
+      participation: {
+        required,
+        integer,
+        between: between(0, 100),
+        maxValue: maxValue(100 - this.$props.overallParticipation)
+      }
     }
   },
   methods: {
+    // Control input fields validation
     send(mutate){
-      mutate();
-      this.firstName = '';
-      this.lastName = '';
-      this.participation = '';
+      if (!this.$v.firstName.$invalid || !this.$v.lastName.$invalid || !this.$v.participation.$invalid){
+        mutate();
+        this.firstName = '';
+        this.lastName = '';
+        this.participation = '';
+        this.$v.firstName.$reset();
+        this.$v.lastName.$reset();
+        this.$v.participation.$reset();
+        
+      }      
     },
+    // Send event to CreatePerson parent for done event on ApolloMutation component
     onDone(){
       this.$emit('done');
     }
@@ -63,8 +173,7 @@ export default {
 }
 input, button {
   border-radius: 3px;
-  padding: 18px;
-  margin: 10px;
+  width: 100%;
 }
 .header, button {
   background-color: #00b8e2;
@@ -75,34 +184,41 @@ input, button {
   padding-bottom: 30px;
 }
 @media only screen and (min-width: 0px) {
-  input {width: 90%}
-  button {width: 60%; font-size: 14px; padding: 10px;}
+  .form-group {width: 90%}
+  .form-control {width: 60%;}
+  button {font-size: 14px; padding: 16px}
 }
 @media only screen and (min-width: 220px) {
-  input {width: 90%}
-  button {width: 40%; font-size: 14px; padding: 10px;}
+  .form-group {width: 90%}
+  .form-control{width: 40%;}
+  button {font-size: 14px; padding: 16px}
 }
 @media only screen and (min-width: 350px) {
-  input {width: 90%}
-  button {width: 25%; font-size: 14px; padding: 10px;}
+  .form-group {width: 90%}
+  .form-control{width: 25%;}
+  button {font-size: 14px; padding: 16px}
 }
 @media only screen and (min-width: 860px) {
-  input {width: 25%}
-  button {width: 15%; font-size: 18px;}
+  .form-group {width: 25%}
+  .form-control{width: 15%;}
+  button {font-size: 17px; padding: 14px}
 }
 @media only screen and (min-width: 1000px) {
-  input {width: 18%}
-  button {width: 8%; font-size: 18px;}
+  .form-group {width: 18%}
+  .form-control{width: 8%;}
+  button {font-size: 17px; padding: 14px}
 }
 @media only screen and (min-width: 1920px) {
-  input {width: 16%}
-  button {width: 8%; font-size: 18px;}
+  .form-group {width: 16%}
+  .form-control{width: 8%;}
+  button {font-size: 17px; padding: 14px}
 }
 input {
   color: rgb(80, 75, 75);
   background-color: #fff;
   font-size: 14px;
   border-width: 0px;
+  padding: 18px;
 }
 button {
   color: #fff;
@@ -111,4 +227,14 @@ button {
   border-width: 2px;
   border-style: solid;
 }
+.form-group, .form-control{
+  margin: 10px;
+}
+.error {
+  padding: 5px;
+  font-size: 13px;
+  font-weight: bold;
+  color: rgb(255, 248, 145);
+}
+
 </style>
