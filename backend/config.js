ccccb041cb4807338af4514ed75cab47c4fc52cb
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-16 17:12:32 -0300

    Fix config.js for production

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 
 const result = dotenv.config({silent: true})
 
-if (result.error) throw result.error;
+if (result.error && !(process.env.NODE_ENV==='production')) throw result.error;
+
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_DRIVE: process.env.DB_DRIVE || 'mongodb',
